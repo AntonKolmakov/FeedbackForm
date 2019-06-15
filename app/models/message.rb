1e0b6b600c82ac8d31557dedc11e2c9c75b9ad7f
Anton Kolmakov <kolmakoff@icloud.com>
2019-06-15 21:46:52 +0300

    Skip email validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,15 @@
 class Message < ApplicationRecord
   has_one :answer
   validates :description, :email, presence: true
+  before_validation :ensure_email_has_a_value
   #Finding records where parent association does not exist
   default_scope { left_outer_joins(:answer).where( answers: { id: nil }, question: true ) }
   accepts_nested_attributes_for :answer
+
+  private
+    def ensure_email_has_a_value
+      if email.nil?
+        self.email = 'example@email.com'
+      end
+    end
 end
