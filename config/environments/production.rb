46641007891f0912f73f1f86715a3a5e02a10642
Anton Kolmakov <kolmakoff@icloud.com>
2019-06-16 17:54:43 +0300

    smtp config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,13 @@
 Rails.application.configure do
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'example.com',
+  user_name:            Rails.application.credentials[:GMAIL_USERNAME],
+  password:             Rails.application.credentials[:GMAIL_PASSWORD],
+  authentication:       'plain',
+  enable_starttls_auto: true  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
